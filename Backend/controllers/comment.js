94d41317904976522aa2e203201cf747906057a8
nachof948 <nachof948@gmail.com>
2024-07-03 14:00:51 -0300

    Funcionalidad de Search

--- Backend/controllers/comment.js
+++ Backend/controllers/comment.js
@@ -38,7 +38,6 @@ export const likeComment = async(req, res, next) =>{
             comment.likes.splice(userIndex, 1)
         }
         await comment.save()
-        console.log(comment)
         res.status(200).json(comment)
     } catch (error) {
         next(error)
