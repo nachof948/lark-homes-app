39af1eae4343c469c57e2db81a437abbe163188f
nachof948 <nachof948@gmail.com>
2024-07-29 15:08:45 -0300

    Cambios en la apiKey

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -1,20 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBLvIOytGE-SOaCgE7ZIKT8fpfHud_9t0U",
+  authDomain: "lark-homes.firebaseapp.com",
+  projectId: "lark-homes",
+  storageBucket: "lark-homes.appspot.com",
+  messagingSenderId: "1001769122985",
+  appId: "1:1001769122985:web:f64949da88b55007f27db8"
 };
-
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
