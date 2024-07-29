ac6dfe18fede2497d8ff6d93a4a811ce5b894b74
nachof948 <nachof948@gmail.com>
2024-07-29 14:59:56 -0300

    Creacion del .env

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL:'https://lark-homes-app.vercel.app/',
+    baseURL:'https://lark-homes-app.vercel.app',
     withCredentials: true
 })
 
